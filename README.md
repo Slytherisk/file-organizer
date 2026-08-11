67af2e3d50dcac0f4f70552c4b3658a6a021481c
Slytherisk <mahan.devsec@gmail.com>
2026-08-11 13:53:19 +0330

    Functional file organizer foundation

---
+++ README.md
@@ -0,0 +1,33 @@
+# File Organizer 
+
+## A simple python script for organizing your files.
+
+---
+
+## Requirements
+
+- Python 3.x
+- No external libraries required
+
+---
+
+## How does it work?
+
+- Run the script in a specific folder.
+- Python code creates folders and moves files into them depending
+  on their formats. Ins: "file.jpg" > Pictures
+
+---
+
+## Current state
+
+-> In progress.
+
+--> future features will be announced in the README.md.
+
+---
+
+## How to run
+
+```bash
+python main.py
---
+++ codes/final/main.py
@@ -0,0 +1,58 @@
+from pathlib import Path
+import shutil
+import os
+import re
+
+list_file = os.listdir()
+for file in list_file:
+    if not(Path.is_dir(file)):
+        format = re.search(r"\.\w+$", file).group()
+    match format:
+        case ".jpg" | ".png" | ".jpeg":
+            try:
+                Path.mkdir("Picture")
+                shutil.move(file, "Picture")
+            except FileExistsError:
+                shutil.move(file, "Picture")
+
+        case ".wav" | ".mp3":
+            try:
+                Path.mkdir("Music")
+                shutil.move(file, "Music")
+            except FileExistsError:
+                shutil.move(file, "Music")
+
+        case ".zip" | ".rar":
+            try:
+                Path.mkdir("Compressed")
+                shutil.move(file, "Compressed")
+            except FileExistsError:
+                shutil.move(file, "Compressed")
+
+        case ".epub":
+            try:
+                Path.mkdir("Book")
+                shutil.move(file, "Book")
+            except FileExistsError:
+                shutil.move(file, "Book")
+
+        case ".exe":
+            try:
+                Path.mkdir("Program")
+                shutil.move(file, "Program")
+            except FileExistsError:
+                shutil.move(file, "Program")
+
+        case ".mp4":
+            try:
+                Path.mkdir("Video")
+                shutil.move(file, "Video")
+            except FileExistsError:
+                shutil.move(file, "Video")
+
+        case ".pdf" | ".PDF" | ".txt":
+            try:
+                Path.mkdir("Document")
+                shutil.move(file, "Document")
+            except:
+                shutil.move(file, "Document")
