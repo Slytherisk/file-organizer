8bf962141d1a751f6350bd4f470f4a465bcdb64c
Slytherisk <mahan.devsec@gmail.com>
2026-08-13 16:38:20 +0330

    Fixing bug: Indenting the match-case block. Added detail: Handle files without common formats.

--- codes/final/main.py
+++ codes/final/main.py
@@ -7,52 +7,60 @@ list_file = os.listdir()
 for file in list_file:
     if not(Path.is_dir(file)):
         format = re.search(r"\.\w+$", file).group()
-    match format:
-        case ".jpg" | ".png" | ".jpeg":
-            try:
-                Path.mkdir("Picture")
-                shutil.move(file, "Picture")
-            except FileExistsError:
-                shutil.move(file, "Picture")
+        match format:
+            case ".jpg" | ".png" | ".jpeg":
+                try:
+                    Path.mkdir("Picture")
+                    shutil.move(file, "Picture")
+                except FileExistsError:
+                    shutil.move(file, "Picture")
 
-        case ".wav" | ".mp3":
-            try:
-                Path.mkdir("Music")
-                shutil.move(file, "Music")
-            except FileExistsError:
-                shutil.move(file, "Music")
+            case ".wav" | ".mp3":
+                try:
+                    Path.mkdir("Music")
+                    shutil.move(file, "Music")
+                except FileExistsError:
+                    shutil.move(file, "Music")
 
-        case ".zip" | ".rar":
-            try:
-                Path.mkdir("Compressed")
-                shutil.move(file, "Compressed")
-            except FileExistsError:
-                shutil.move(file, "Compressed")
+            case ".zip" | ".rar":
+                try:
+                    Path.mkdir("Compressed")
+                    shutil.move(file, "Compressed")
+                except FileExistsError:
+                    shutil.move(file, "Compressed")
 
-        case ".epub":
-            try:
-                Path.mkdir("Book")
-                shutil.move(file, "Book")
-            except FileExistsError:
-                shutil.move(file, "Book")
+            case ".epub":
+                try:
+                    Path.mkdir("Book")
+                    shutil.move(file, "Book")
+                except FileExistsError:
+                    shutil.move(file, "Book")
 
-        case ".exe":
-            try:
-                Path.mkdir("Program")
-                shutil.move(file, "Program")
-            except FileExistsError:
-                shutil.move(file, "Program")
+            case ".exe":
+                try:
+                    Path.mkdir("Program")
+                    shutil.move(file, "Program")
+                except FileExistsError:
+                    shutil.move(file, "Program")
 
-        case ".mp4":
-            try:
-                Path.mkdir("Video")
-                shutil.move(file, "Video")
-            except FileExistsError:
-                shutil.move(file, "Video")
+            case ".mp4":
+                try:
+                    Path.mkdir("Video")
+                    shutil.move(file, "Video")
+                except FileExistsError:
+                    shutil.move(file, "Video")
 
-        case ".pdf" | ".PDF" | ".txt":
-            try:
-                Path.mkdir("Document")
-                shutil.move(file, "Document")
-            except:
-                shutil.move(file, "Document")
+            case ".pdf" | ".PDF" | ".txt":
+                try:
+                    Path.mkdir("Document")
+                    shutil.move(file, "Document")
+                except:
+                    shutil.move(file, "Document")
+
+            case _:
+                if file != "main.py":
+                    try:
+                        Path.mkdir("Unknown")
+                        shutil.move(file, "Unknown")
+                    except:
+                        shutil.move(file, "Unknown")
